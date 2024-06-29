8c314f90ddaa543cee249ef09a264fb6638014f2
lhldanh <120589546+lhldanh@users.noreply.github.com>
2024-06-30 00:36:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,25 @@ def load_vocab(file_path):
     return words
 
 
-def levenshtein_distance(src, target):
-    m = len(src)
-    n = len(target)
-    dp = [[0 for _ in range(n + 1)] for i in range(m + 1)]
-
+def levenshteinFullMatrix(str1, str2):
+    m = len(str1)
+    n = len(str2)
+ 
+    dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+ 
     for i in range(m + 1):
         dp[i][0] = i
     for j in range(n + 1):
         dp[0][j] = j
-
+ 
     for i in range(1, m + 1):
         for j in range(1, n + 1):
-            if (src[i - 1] == target[j - 1]):
-                dp[i][j] = dp[i-1][j-1]
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
             else:
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-
+                dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+ 
     return dp[m][n]
-
 
 def main():
     st.title('Word Correction using Levenshtein Distance')
